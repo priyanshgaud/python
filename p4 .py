82d2338c7948f9460c65ff0dc85d3b7157964c55
priyanshgaud <priyanshgaud71@gmail.com>
2025-07-04 19:33:13 +0530

    Add files via upload

---
+++ p4 .py
@@ -0,0 +1,9 @@
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+course = input("Enter your course: ")
+intro = f"""
+my name is {name}
+i am {age} year old
+i studying {course} course
+"""
+print(intro)
